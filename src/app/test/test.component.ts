df6cf1c8bd295dc5450126b152142d2d02b04673
elamurugan45 <elamuruganraju4545@gmail.com>
2024-09-12 11:48:56 +0530

    binding completed

--- src/app/test/test.component.ts
+++ src/app/test/test.component.ts
@@ -1,13 +1,17 @@
 import { Component, OnInit } from '@angular/core';
 
 @Component({
-  selector: '[app-test]',
+  selector: 'app-test',
   standalone: true,
   template: `
-    <h2>welocme {{ name }}</h2>
+    <h2>welcome {{ name }}</h2>
     <h2 class="text-success">Code Test</h2>
     <h2 [class]="successClass">Code Test</h2>
     <h2 class="text-danger">Code Test</h2>
+    <input #myInput type="text">
+    <button (click)="logMessage(myInput.value)">Greet</button>
+    <input  type="text">
+    {{bindingName}}
   `,
   styles: [
     `
@@ -22,10 +26,16 @@ import { Component, OnInit } from '@angular/core';
       }
     `,
   ],
+  // templateUrl:'./test.Component.html'
 })
 export class TestComponent implements OnInit {
   public name = 'King Ragnar';
   public successClass = 'text-success';
+  public greeting = "";
+  public bindingName =""
   constructor() {}
   ngOnInit() {}
+  logMessage(value:any){
+    console.log(value)
+  }
 }
